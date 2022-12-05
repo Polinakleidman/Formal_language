040018b8f21b2490dc368ce7e62fbee6e48bac1f
Polina <kleidman.pd@phystech.edu>
2022-12-05 14:39:54 +0300

    removed bad forders

--- formal_lang/cmake-build-debug-coverage/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/polly/CLionProjects/formal_lang
-# Build directory: C:/Users/polly/CLionProjects/formal_lang/cmake-build-debug-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/polly/CLionProjects/formal_lang/cmake-build-debug-coverage/unit_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- formal_lang/cmake-build-debug-coverage/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/polly/CLionProjects/formal_lang/cmake-build-debug-coverage/_deps/googletest-src/googletest
-# Build directory: C:/Users/polly/CLionProjects/formal_lang/cmake-build-debug-coverage/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
